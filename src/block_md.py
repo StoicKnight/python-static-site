c4c2382fa1f940be789835e814005c1f56e0720c
Stoic Knight <alexis.skouris@pm.me>
2025-11-26 22:20:30 +0200

    reformat: the markdown to html nodes functions

--- src/block_md.py
+++ src/block_md.py
@@ -6,169 +6,116 @@ from src.htmlnode import ParentNode
 from src.textnode import TextNode, TextType, text_node_to_html_node
 
 
-def md_to_blocks(markdown_text: str):
-    blocks = markdown_text.split("\n\n")
-    result = []
-    is_code_block = False
-    code_block = ""
-    for block in blocks:
-        if block.startswith("```"):
-            is_code_block = not is_code_block
-        if is_code_block:
-            code_block += f"{block}\n\n"
-            if block.endswith("```"):
-                is_code_block = not is_code_block
-                result.append(code_block.rstrip("\n"))
-                code_block = ""
-            continue
-        if block == "":
-            continue
-        block = block.strip()
-        result.append(block)
-    return result
+RE_HEADING = re.compile(r"^#{1,6}\s(.*)$", re.MULTILINE)
+RE_QUOTE = re.compile(r"^>\s?(.*)$", re.MULTILINE)
+RE_UL_ITEM = re.compile(r"^[-*]\s(.*)$", re.MULTILINE)
+RE_OL_ITEM = re.compile(r"^\d\.\s(.*)$", re.MULTILINE)
+RE_CODE_BLOCK = re.compile(r"\`{3}(?:\w+)?\n([^\`]+)\n\`{3}", re.MULTILINE)
 
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
-    HEADING_1 = "heading_1"
-    HEADING_2 = "heading_2"
-    HEADING_3 = "heading_3"
-    HEADING_4 = "heading_4"
-    HEADING_5 = "heading_5"
-    HEADING_6 = "heading_6"
+    HEADING = "heading"
     CODE = "code"
     QUOTE = "quote"
     UNORDERED_LIST = "unordered_list"
     ORDERED_LIST = "ordered_list"
 
 
-def block_to_block_type(block: str):
+def md_to_blocks(markdown_text: str) -> list[str]:
+    raw_blocks = markdown_text.split("\n\n")
+    result = []
+    buffer = []
+    is_in_code_block = False
+    for block in raw_blocks:
+        if block.startswith("```"):
+            is_in_code_block = not is_in_code_block
+        if is_in_code_block:
+            buffer.append(block)
+            if block.endswith("```") and block != buffer[0]:
+                is_in_code_block = not is_in_code_block
+                full_code = "\n\n".join(buffer)
+                result.append(full_code.rstrip("\n"))
+                buffer = []
+            continue
+        if not block:
+            continue
+        result.append(block.strip())
+    return result
+
+
+def block_to_block_type(block: str) -> BlockType:
     lines = block.split("\n")
-    if block.startswith("# "):
-        return BlockType.HEADING_1
-    elif block.startswith("## "):
-        return BlockType.HEADING_2
-    elif block.startswith("### "):
-        return BlockType.HEADING_3
-    elif block.startswith("#### "):
-        return BlockType.HEADING_4
-    elif block.startswith("##### "):
-        return BlockType.HEADING_5
-    elif block.startswith("###### "):
-        return BlockType.HEADING_6
-    elif len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+    if block.startswith("#"):
+        count = len(block) - len(block.lstrip("#"))
+        if 1 <= count <= 6 and block[count : count + 1] == " ":
+            return BlockType.HEADING
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
         return BlockType.CODE
-    elif block.startswith("> "):
-        for line in lines:
-            if not line.startswith("> "):
-                return BlockType.PARAGRAPH
+    if block.startswith("> ") and all(line.startswith("> ") for line in lines):
         return BlockType.QUOTE
-    elif block.startswith("- "):
-        for line in lines:
-            if not line.startswith("- "):
-                return BlockType.PARAGRAPH
+    if block.startswith("- ") and all(line.startswith("- ") for line in lines):
         return BlockType.UNORDERED_LIST
-    elif block.startswith(
-        ("1. ", "2. ", "3. ", "4. ", "5. ", "6. ", "7. ", "8. ", "9. ")
+    ordered_prefixes = tuple(f"{i}. " for i in range(1, 10))
+    if block.startswith(ordered_prefixes) and all(
+        line.startswith(ordered_prefixes) for line in lines
     ):
-        for line in lines:
-            if not line.startswith(
-                ("1. ", "2. ", "3. ", "4. ", "5. ", "6. ", "7. ", "8. ", "9. ")
-            ):
-                return BlockType.PARAGRAPH
         return BlockType.ORDERED_LIST
-    else:
-        return BlockType.PARAGRAPH
+    return BlockType.PARAGRAPH
 
 
-def md_to_html_node(markdown_text):
-    nodes = []
+def md_to_html_node(markdown_text: str) -> ParentNode:
+    children = []
     blocks = md_to_blocks(markdown_text)
     for block in blocks:
         block_type = block_to_block_type(block)
-        nodes.append(create_html_node(block_type, block))
-    return ParentNode(tag="div", children=nodes, props=None)
+        html_node = block_to_html_node(block_type, block)
+        children.append(html_node)
+    return ParentNode(tag="div", children=children, props=None)
 
 
-def create_html_node(block_type, block_text):
-    clean_text = clean_block_text(block_type, block_text)
-    if block_type != BlockType.CODE:
-        children = text_to_children(clean_text)
-        parent = children_to_parent(children, block_type)
-    else:
-        code_text_node = TextNode(text=clean_text, text_type=TextType.CODE)
-        code_html_node = text_node_to_html_node(code_text_node)
-        parent = ParentNode(tag="pre", children=[code_html_node])
-    return parent
+def block_to_html_node(block_type: BlockType, block_text: str) -> ParentNode:
+    match block_type:
+        case BlockType.HEADING:
+            level = len(block_text) - len(block_text.lstrip("#"))
+            text = clean_block_text(block_type, block_text)
+            children = text_to_children(text)
+            return ParentNode(tag=f"h{level}", children=children)
+        case BlockType.CODE:
+            text = clean_block_text(block_type, block_text)
+            code_node = TextNode(text=text, text_type=TextType.CODE)
+            return ParentNode(tag="pre", children=[text_node_to_html_node(code_node)])
+        case BlockType.UNORDERED_LIST:
+            items = RE_UL_ITEM.findall(block_text)
+            list_nodes = [ParentNode("li", text_to_children(item)) for item in items]
+            return ParentNode(tag="ul", children=list_nodes)
+        case BlockType.ORDERED_LIST:
+            items = RE_OL_ITEM.findall(block_text)
+            list_nodes = [ParentNode("li", text_to_children(item)) for item in items]
+            return ParentNode(tag="ol", children=list_nodes)
+        case BlockType.QUOTE:
+            text = clean_block_text(block_type, block_text)
+            children = text_to_children(text)
+            return ParentNode(tag="blockquote", children=children)
+        case _:
+            text = clean_block_text(block_type, block_text)
+            children = text_to_children(text)
+            return ParentNode(tag="p", children=children)
 
 
-def clean_block_text(block_type, block_text):
-    text = ""
-    if block_type in [
-        BlockType.HEADING_1,
-        BlockType.HEADING_2,
-        BlockType.HEADING_3,
-        BlockType.HEADING_4,
-        BlockType.HEADING_5,
-        BlockType.HEADING_6,
-    ]:
-        text = "\n".join(re.findall(r"^#{1,6}\s(.*)", block_text, flags=re.MULTILINE))
-    elif block_type == BlockType.UNORDERED_LIST:
-        text = "".join(re.findall(r"^[-*]\s?(.*)$", block_text, flags=re.MULTILINE))
-    elif block_type == BlockType.ORDERED_LIST:
-        text = "".join(re.findall(r"^\d\.\s?(.*)$", block_text, flags=re.MULTILINE))
-    elif block_type == BlockType.QUOTE:
-        text = "\n".join(re.findall(r"^>\s?(.*)$", block_text, flags=re.MULTILINE))
-    elif block_type == BlockType.CODE:
-        text = "\n".join(
-            re.findall(
-                r"\`{3}(?:\w+)?\n([^\`]+)\n\`{3}", block_text, flags=re.MULTILINE
-            )
-        )
-    else:
-        text = block_text
-
-    return text.strip()
+def clean_block_text(block_type: BlockType, block_text: str) -> str:
+    match block_type:
+        case BlockType.HEADING:
+            matches = RE_HEADING.findall(block_text)
+            return matches[0] if matches else block_text.lstrip("#").strip()
+        case BlockType.QUOTE:
+            return "\n".join(RE_QUOTE.findall(block_text))
+        case BlockType.CODE:
+            return "\n".join(RE_CODE_BLOCK.findall(block_text))
+        case _:
+            return block_text.strip()
 
 
-def text_to_children(text):
-    html_nodes = []
-    print(f"Text:\n{text}")
+def text_to_children(text: str) -> list:
     text_nodes = text_to_textnodes(text)
-    print(text_nodes)
-    for node in text_nodes:
-        html_nodes.append(text_node_to_html_node(node))
-    return html_nodes
-
-
-def children_to_parent(children, block_type):
-    parent = None
-    if block_type == BlockType.HEADING_1:
-        parent = ParentNode(tag="h1", children=children)
-    if block_type == BlockType.HEADING_2:
-        parent = ParentNode(tag="h2", children=children)
-    if block_type == BlockType.HEADING_3:
-        parent = ParentNode(tag="h3", children=children)
-    if block_type == BlockType.HEADING_4:
-        parent = ParentNode(tag="h4", children=children)
-    if block_type == BlockType.HEADING_5:
-        parent = ParentNode(tag="h5", children=children)
-    if block_type == BlockType.HEADING_6:
-        parent = ParentNode(tag="h6", children=children)
-    elif block_type == BlockType.ORDERED_LIST:
-        list_nodes = []
-        for child in children:
-            list_node = ParentNode(tag="li", children=[child])
-            list_nodes.append(list_node)
-        parent = ParentNode(tag="ol", children=list_nodes)
-    elif block_type == BlockType.UNORDERED_LIST:
-        list_nodes = []
-        for child in children:
-            list_node = ParentNode(tag="li", children=[child])
-            list_nodes.append(list_node)
-        parent = ParentNode(tag="ul", children=list_nodes)
-    elif block_type == BlockType.QUOTE:
-        parent = ParentNode(tag="blockquote", children=children)
-    elif block_type == BlockType.PARAGRAPH:
-        parent = ParentNode(tag="p", children=children)
-    return parent
+    return [text_node_to_html_node(node) for node in text_nodes]
--- src/tests/test_block_md.py
+++ src/tests/test_block_md.py
@@ -77,12 +77,12 @@ def test_md_to_blocks(text, expected):
 @pytest.mark.parametrize(
     "text, expected",
     [
-        ("# Heading 1", BlockType.HEADING_1),
-        ("## Heading 2", BlockType.HEADING_2),
-        ("### Heading 3", BlockType.HEADING_3),
-        ("#### Heading 4", BlockType.HEADING_4),
-        ("##### Heading 5", BlockType.HEADING_5),
-        ("###### Heading 6", BlockType.HEADING_6),
+        ("# Heading 1", BlockType.HEADING),
+        ("## Heading 2", BlockType.HEADING),
+        ("### Heading 3", BlockType.HEADING),
+        ("#### Heading 4", BlockType.HEADING),
+        ("##### Heading 5", BlockType.HEADING),
+        ("###### Heading 6", BlockType.HEADING),
         (
             """This a paragraph. This is a test text.
 That spans multiple Lines.
@@ -172,23 +172,93 @@ if __name__ = __main__:
 
 #### Heading 4
 
-- _Italic_
-- Text
+- This is _Italic_ item
+- more items with `code`
 - [Link Item](https://link.to.item.com)
 
 ##### Heading 5
 
-1. Text
-2. **BOLD**
-3. `code`
+1. Item one
+2. **BOLD** item 2
+3. `code` item number three
 
 ###### Heading 6
 
 >Not a quote""",
-            """<div><h1>Heading 1</h1><p>This is a summary for the document</p><h2>Heading 2</h2><p>This a paragraph. This is a <b>test</b> text.\nThat spans multiple Lines.\n<img src="/path/to/image" alt="image"></img>.</p><blockquote>This is a Quote\nand more quote</blockquote><h3>Heading 3</h3><pre><code>import json\n\ndef main():\n\tprint("Hello World")\n\nif __name__ = __main__:\n\tmain()</code></pre><h4>Heading 4</h4><ul><li><i>Italic</i></li><li>Text</li><li><a href="https://link.to.item.com">Link Item</a></li></ul><h5>Heading 5</h5><ol><li>Text</li><li><b>BOLD</b></li><li><code>code</code></li></ol><h6>Heading 6</h6><p>>Not a quote</p></div>""",
-        )
+            """<div><h1>Heading 1</h1><p>This is a summary for the document</p><h2>Heading 2</h2><p>This a paragraph. This is a <b>test</b> text.\nThat spans multiple Lines.\n<img src="/path/to/image" alt="image"></img>.</p><blockquote>This is a Quote\nand more quote</blockquote><h3>Heading 3</h3><pre><code>import json\n\ndef main():\n\tprint("Hello World")\n\nif __name__ = __main__:\n\tmain()</code></pre><h4>Heading 4</h4><ul><li>This is <i>Italic</i> item</li><li>more items with <code>code</code></li><li><a href="https://link.to.item.com">Link Item</a></li></ul><h5>Heading 5</h5><ol><li>Item one</li><li><b>BOLD</b> item 2</li><li><code>code</code> item number three</li></ol><h6>Heading 6</h6><p>>Not a quote</p></div>""",
+        ),
+        (
+            """
+This is **bolded** paragraph
+text in a p
+tag here
+
+""",
+            "<div><p>This is <b>bolded</b> paragraph\ntext in a p\ntag here</p></div>",
+        ),
+        (
+            """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+""",
+            "<div><p>This is <b>bolded</b> paragraph\ntext in a p\ntag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        ),
+        (
+            """
+- This is a list
+- with items
+- and _more_ items
+
+1. This is an `ordered` list
+2. with items
+3. and more items
+
+""",
+            "<div><ul><li>This is a list</li><li>with items</li><li>and <i>more</i> items</li></ul><ol><li>This is an <code>ordered</code> list</li><li>with items</li><li>and more items</li></ol></div>",
+        ),
+        (
+            """
+# this is an h1
+
+this is paragraph text
+
+## this is an h2
+""",
+            "<div><h1>this is an h1</h1><p>this is paragraph text</p><h2>this is an h2</h2></div>",
+        ),
+        (
+            """
+> This is a
+> blockquote block
+
+this is paragraph text
+
+""",
+            "<div><blockquote>This is a\nblockquote block</blockquote><p>this is paragraph text</p></div>",
+        ),
+        (
+            """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+""",
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff</code></pre></div>",
+        ),
     ],
-    ids=["test"],
+    ids=[
+        "full markdown",
+        "paragraph",
+        "multiple paragraphs",
+        "lists",
+        "headings",
+        "blockquote",
+        "code",
+    ],
 )
 def test_md_to_html_node(markdown, expected):
     actual = md_to_html_node(markdown).to_html()
